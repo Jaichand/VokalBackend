e300f7706c4f1b9d3987a45cde6e43e1522900c3
jaichand <jaysamriya21@gmail.com>
2017-05-14 14:57:46 +0530

    added models and needed packages

---
+++ models/location.js
@@ -0,0 +1,25 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var minute = require('mongoose-minute');
+
+location = new Schema({
+  name : {
+    type: String
+  },
+  location: {
+    type:{
+      type: String
+    },
+    coordinates: [Number]
+  }
+  createdUser: {
+    _id: mongoose.Schema.Types.ObjectId
+  }
+});
+
+minute(location, {
+  createdAt: 'createdAt'
+});
+
+Locations = mongoose.model('location', location);
+model.exports = Locations;
